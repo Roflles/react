24b963634ea81dbf689f4f4a9a127c7c23db833d
Roflles <igxfrunza@gmail.com>
2023-05-30 15:33:10 +0100

    Weather fihish

--- cities/src/componets/Weather.js
+++ cities/src/componets/Weather.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Weather = (props) => {
   return (
-    <div>
+    <div className='infoWeath'>
       {props.citi && 
         <div>
           <p>Росташування: {props.citi}, {props.country}</p>
@@ -11,7 +11,7 @@ const Weather = (props) => {
           <p>Захід сонця: {props.sunset}</p>
         </div>
       }
-        <p>{ props.error }</p>
+        <p className='error'>{ props.error }</p>
     </div>
   );
 }
