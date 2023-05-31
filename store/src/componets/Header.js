ce3ce71b1e424ff71ba7efc43d876e9674ce200e
Roflles <igxfrunza@gmail.com>
2023-05-31 17:14:50 +0100

    31/05

---
+++ store/src/componets/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <span className='logo'>House Staff</span>
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
+
+export default Header
